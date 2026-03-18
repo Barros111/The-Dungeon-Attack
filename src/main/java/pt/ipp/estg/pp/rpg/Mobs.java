1c5d0c391af91abbc50aee737d0214d38088507a
Barros111 <bxrrosbusiness@gmail.com>
2026-03-18 19:02:14 +0000

    Removed mobTypes

--- src/main/java/pt/ipp/estg/pp/rpg/Mobs.java
+++ src/main/java/pt/ipp/estg/pp/rpg/Mobs.java
@@ -2,19 +2,11 @@ package pt.ipp.estg.pp.rpg;
 
 public class Mobs extends Character{
 
-    public enum mobType{Goblin, Ghost};
-
-    private mobType mobType;
     private int value; //1-5 1 is low value, 5 is high value.
 
-    public Mobs(String name, mobType mobType, int value){
+    public Mobs(String name, int value){
         super(name);
-        this.mobType = mobType;
         this.value = value;
-    }
-
-    public mobType getMobType(){
-        return mobType;
     }
 
     public int getValue(){
